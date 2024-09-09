f7a15082b6daddb026b02ff16d5c404248a34fd9
mohanad-hafez <mohanadmohamed474@gmail.com>
2024-09-09 23:02:15 +0300

    implement reverse and equal methods

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -30,9 +30,23 @@ public class String1 {
             append(s.getChar(i));
     }
     public boolean equal(String1 s) {
-        throw new UnsupportedOperationException("equal() function is not supported yet.");
+        if (self.length() != s.length()) {
+            return false;
+        }
+
+        for (int i = 0; i <= size; i++) {
+            if (str[i] != s.getChar(i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
     public void reverse() {
-        throw new UnsupportedOperationException("reverse() function is not supported yet.");
+        for (int i = 0; i <= size/2; i++) {
+            char temp = str[i];
+            str[i] = str[size-i];
+            str[size-i] = temp;
+        }
     }
 }
